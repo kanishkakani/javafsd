73ef65150ef2292f5bd08486e33d9f66c2885e9c
kanishkakani <109128177+kanishkakani@users.noreply.github.com>
2022-08-11 15:07:29 +0530

    changes

---
+++ Assisted phase 2/bookstorewebapp/src/main/java/com/samples/servlet/BookServlet.java
@@ -0,0 +1,42 @@
+package com.samples.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.samples.domain.Book;
+import com.samples.service.BookstoreService;
+
+@WebServlet("/books")
+public class BookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	BookstoreService bookstoreService;
+	
+	public void init() throws ServletException {
+		super.init();
+		this.bookstoreService = new BookstoreService();
+	}
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Book> books = this.bookstoreService.getBooks();
+		
+		request.setAttribute("books", books);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/viewbooks.jsp");
+		rd.forward(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
